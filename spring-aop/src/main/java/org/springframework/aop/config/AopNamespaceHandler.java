07059f76705d4b05b7e7adae2918e38df56dc0cd
wangc <1969877890@qq.com>
2022-09-19 23:59:04 +0800

    了解AOP--AopNamespaceHandler

--- spring-aop/src/main/java/org/springframework/aop/config/AopNamespaceHandler.java
+++ spring-aop/src/main/java/org/springframework/aop/config/AopNamespaceHandler.java
@@ -21,22 +21,21 @@ import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
 /**
- * {@code NamespaceHandler} for the {@code aop} namespace.
+ * {@code NamespaceHandler}用于{@code aop} namespace.
  *
- * <p>Provides a {@link org.springframework.beans.factory.xml.BeanDefinitionParser} for the
- * {@code <aop:config>} tag. A {@code config} tag can include nested
- * {@code pointcut}, {@code advisor} and {@code aspect} tags.
+ * <p>为{@code <aop:config>}标签提供
+ * {@link org.springframework.beans.factory.xml.BeanDefinitionParser}.
+ * {@code config}标签可以包括嵌套的{@code pointcut}, {@code advisor}和
+ * {@code aspect}标签.
  *
- * <p>The {@code pointcut} tag allows for creation of named
- * {@link AspectJExpressionPointcut} beans using a simple syntax:
+ * <p>{@code pointcut}标签允许使用简单的语法创建命名{@link AspectJExpressionPointcut} beans:
  * <pre class="code">
  * &lt;aop:pointcut id=&quot;getNameCalls&quot; expression=&quot;execution(* *..ITestBean.getName(..))&quot;/&gt;
  * </pre>
  *
- * <p>Using the {@code advisor} tag you can configure an {@link org.springframework.aop.Advisor}
- * and have it applied to all relevant beans in you {@link org.springframework.beans.factory.BeanFactory}
- * automatically. The {@code advisor} tag supports both in-line and referenced
- * {@link org.springframework.aop.Pointcut Pointcuts}:
+ * <p>使用{@code advisor}标签, 您可以配置{@link org.springframework.aop.Advisor},
+ * 并将其自动应用到{@link org.springframework.beans.factory.BeanFactory}中所有相关bean.
+ * {@code advisor}表现支持内联和引用{@link org.springframework.aop.Pointcut Pointcuts}:
  *
  * <pre class="code">
  * &lt;aop:advisor id=&quot;getAgeAdvisor&quot;
@@ -55,18 +54,17 @@ import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 public class AopNamespaceHandler extends NamespaceHandlerSupport {
 
 	/**
-	 * Register the {@link BeanDefinitionParser BeanDefinitionParsers} for the
-	 * '{@code config}', '{@code spring-configured}', '{@code aspectj-autoproxy}'
-	 * and '{@code scoped-proxy}' tags.
+	 * 为'{@code config}', '{@code spring-configured}', '{@code aspectj-autoproxy}'
+	 * 和'{@code scoped-proxy}'标签注册{@link BeanDefinitionParser BeanDefinitionParsers}.
 	 */
 	@Override
 	public void init() {
-		// In 2.0 XSD as well as in 2.5+ XSDs
+		// 在2.0 XSD和 2.5+ XSDs中
 		registerBeanDefinitionParser("config", new ConfigBeanDefinitionParser());
 		registerBeanDefinitionParser("aspectj-autoproxy", new AspectJAutoProxyBeanDefinitionParser());
 		registerBeanDefinitionDecorator("scoped-proxy", new ScopedProxyBeanDefinitionDecorator());
 
-		// Only in 2.0 XSD: moved to context namespace in 2.5+
+		// 仅在2.0 XSD: 在2.5+中移动到context命名空间
 		registerBeanDefinitionParser("spring-configured", new SpringConfiguredBeanDefinitionParser());
 	}
 
